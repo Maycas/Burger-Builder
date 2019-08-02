2fff074fdfe3aa35e173cf70508f149fb53fe085
Marc Maycas <marc.maycas@gmail.com>
2019-08-02 17:56:34 +0200

    Added Navigation Bar

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -21,6 +21,7 @@ const orderSummary = (props) => {
         {ingredientsSummary}
       </ul>
       <p>Continue to Checkout?</p>
+      <p><strong>Total Price: {props.price.toFixed(2)} $</strong></p>
       <Button btnType='Danger' clicked={props.purchaseCancelled}>CANCEL</Button>
       <Button btnType='Success' clicked={props.purchaseContinued}>CONFIRM</Button>
     </Aux>
